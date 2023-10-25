4df2ebeb6ec1c2c2cec7f3a073a90edc3592b897
Gabriel-Delazeri <gbldelazeri@gmail.com>
2023-10-25 10:53:02 -0300

    fix: jpa entity configuration

--- src/main/java/com/delazeri/cleanarch/infrastructure/persistence/jpa/MusicEntity.java
+++ src/main/java/com/delazeri/cleanarch/infrastructure/persistence/jpa/MusicEntity.java
@@ -1,5 +1,8 @@
 package com.delazeri.cleanarch.infrastructure.persistence.jpa;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -8,8 +11,10 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class MusicEntity {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
     private String slug;
